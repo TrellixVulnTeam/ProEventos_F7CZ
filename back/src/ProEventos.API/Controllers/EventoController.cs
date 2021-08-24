3067a1a152b6471a8650d15c859566094de31647
shigueaki <44611671+shigueaki@users.noreply.github.com>
2021-08-24 17:13:52 -0300

    First Commit

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        private readonly DataContext Context;
+        public EventoController(DataContext context)
+        {
+            this.Context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return Context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id) 
+        {
+           return Context.Eventos.Where(value => value.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post!";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put = {id}";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Exemplo de Delete!";
+        }
+    }
+}
